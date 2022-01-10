c23ce389bd1b5e010f26184ae74c3bba5fc026ce
Henrique Medeiros <rique.medeirosneves@gmail.com>
2022-01-09 21:49:01 -0300

    Criação do Carrosel de Filmes em Cartaz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react"
 import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Header from "./components/Header/Header"
 import Movies from "./components/Movies/Movies"
+import Carousel from "./components/Carousel/Carousel"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App () {
@@ -10,7 +13,7 @@ function App () {
         <BrowserRouter>
             <Header/>
             <Routes>
-                <Route path="/" element={<h1>Página Inicial</h1>}/>
+                <Route path="/" element={<Carousel/>}/>
                 <Route path="/programacao" element={<Movies/>}/> 
                 <Route path="/bomboniere" />
                 <Route path="/unidades"/>          
