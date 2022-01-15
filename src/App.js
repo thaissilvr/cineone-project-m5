bc8bca95aec08a0effda8b46e07c0174455fe46e
Yara Proença <yara.proenca.28@gmail.com>
2022-01-14 23:16:21 -0300

    :sparkles: Work done!
    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Header from "./components/Header/Header";
-// import Movies from "./components/Movies/Movies";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
